cba158edfe1b05c769be57fc96ea1e0bca354f8c
homemix <kenmutati@gmail.com>
2021-08-31 06:19:59 +0000

    1. I sometimes suffer from insomnia.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - prints lowercase alphabets
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
