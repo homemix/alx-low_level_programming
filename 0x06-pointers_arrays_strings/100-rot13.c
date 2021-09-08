50a784c45dd0d3bf7edc67ab3812c493e3f30ef8
homemix <kenmutati@gmail.com>
2021-09-08 08:16:38 +0000

    8. rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *rot13 - Rotate letters
+ *@s1: pointer parameter
+ *Description: Rotate letters
+ *Return: return pointer
+ */
+char *rot13(char *s1)
+{
+	int j;
+	char *pString = s1;
+
+	char encodes[2][53] = {
+	{"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"},
+	{"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"}
+				};
+
+	while (*pString != '\0')
+	{
+		char letter = *pString;
+
+		for (j = 0; j < 52; j++)
+		{
+			if (letter == encodes[0][j])
+			{
+				*pString = encodes[1][j];
+			}
+		}
+		pString++;
+	}
+	return (s1);
+}
