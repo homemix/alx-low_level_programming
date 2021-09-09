7faae0e822d4664a49dd6b1181da949ec7c149a7
homemix <kenmutati@gmail.com>
2021-09-09 05:05:16 +0000

    correct qst 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,35 +1,23 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
+ * print_number - Print the given int one digit at a time
+ * @n: The integer to print
  */
 void print_number(int n)
 {
-	unsigned int m, d, count;
-
-	if (n < 0)
+	if (n == 0)
 	{
-		_putchar(45);
-		m = n * -1;
+		_putchar('0');
+		return;
 	}
+	else if (n > 0)
+		n *= -1;
 	else
+		_putchar('-');
+	if ((n / 10) != 0)
 	{
-		m = n;
+		print_number((n / 10) * -1);
 	}
-
-	d = m;
-	count = 1;
-
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	_putchar((n % 10) * -1 + '0');
 }
