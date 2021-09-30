43a8a8d3bab54111e1832e512557d8389ed74bc2
homemix <kenmutati@gmail.com>
2021-09-30 18:03:05 +0000

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,88 +1,50 @@
+#include <stdarg.h>
+#include <stdio.h>
 #include "variadic_functions.h"
-#include<stdio.h>
-#include<stdarg.h>
+
 /**
- * p_char - print char
- * @list:arg
+ * print_all - function that prints anything
+ * @format: character pointer constant
+ *
  * Return: void
  */
 
-void p_char(va_list list)
-{
-	printf("%c", va_arg(list, int));
-}
-/**
- * p_string - print string
- * @list:arg
- * Return: void
- */
-
-void p_string(va_list list)
-{
-	char *str;
-
-	str = va_arg(list, char*);
-	if (str == NULL)
-		str = "(nil)";
-printf("%s", str);
-}
-/**
- * p_integer - print integer
- * @list:arg
- * Return: void
- */
-
-void p_integer(va_list list)
-{
-	printf("%i", va_arg(list, int));
-}
-/**
- * p_float - print float
- * @list:arg
- * Return: void
- */
-
-void p_float(va_list list)
-{
-	printf("%f", va_arg(list, double));
-}
-/**
- * print_all - print everything
- * @format:arg
- * Return: void
- */
 void print_all(const char * const format, ...)
-
 {
-	unsigned int i, j;
-	t_print t[] = {
-		{"c", p_char},
-		{"s", p_string},
-		{"i", p_integer},
-		{"f", p_float},
-		{NULL, NULL}
-	};
-	va_list valist;
-	char *s = "";
+	va_list var;
+	int i = 0;
+	char *separator = ", ", *strTemp = "(nil)", *temp;
 
-	va_start(valist, format);
-	i = 0;
-	while (format && format[i])
+	va_start(var, format);
+
+	while (format != NULL && format[i] != '\0')
 	{
-		j = 0;
-		while (t[j].x != NULL)
+		switch (format[i])
 		{
-			if (*(t[j].x) == format[i])
-			{
-				printf("%s", s);
-				t[j].T_func(valist);
-				s = ", ";
+			case 'c':
+				printf("%c", va_arg(var, int));
 				break;
-			}
-			j++;
+			case 'i':
+				printf("%d", va_arg(var, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(var, double));
+				break;
+			case 's':
+				temp = va_arg(var, char *);
+				if (temp != NULL)
+					strTemp = temp;
+				printf("%s", strTemp);
+				break;
+			default:
+				i++;
+				continue;
 		}
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||
+			format[i] == 's') && format[i + 1] != '\0')
+			printf("%s", separator);
 		i++;
 	}
-	va_end(valist);
 	printf("\n");
+	va_end(var);
 }
