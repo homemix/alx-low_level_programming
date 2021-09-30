81abef0792f6b00fb9114bc88a1cdc6f8968d804
homemix <kenmutati@gmail.com>
2021-09-30 17:33:10 +0000

    add new friles and qst 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: num of args, int
+ *
+ * Return: int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	int i = 0, sum = 0;
+	va_list var;
+
+	va_start(var, n);
+
+	for (i = 0; i < (int) n; i++)
+		sum += va_arg(var, int);
+	va_end(var);
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+a variadic functions assignment
